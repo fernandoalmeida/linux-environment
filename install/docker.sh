652b88d2b2f02556dc7664222cc45fe0c4785ddb
Fernando Almeida <fernando@fernandoalmeida.net>
2015-05-01 23:17:59 -0300

    Fix `docker.sh`

--- install/docker.sh
+++ install/docker.sh
@@ -4,10 +4,10 @@
 ## Docker
 #
 wget -qO- https://get.docker.com/ | sh
-sudo usermod -aG docker $USER
+sudo usermod -aG docker `id -un`
 
 #
 ## Compose
 #
-sudo curl -L https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
