ed4b293cd42aa3f56d1092efe71e72febc0df3b4
Fernando Almeida <fernando@fernandoalmeida.net>
2015-06-08 22:34:10 -0300

    Add Lua Rocks config

--- install/lua.sh
+++ install/lua.sh
@@ -1,3 +1,17 @@
 #!/bin/sh -e
 
-sudo apt-get install lua5.2
+: ${LUA_VERSION:="5.2"}
+: ${LUA_ROCKS_VERSION:="2.2.1"}
+: ${TMP:="$(pwd)/tmp"}
+
+#
+## Lua
+#
+sudo apt-get install lua$LUA_VERSION liblua$LUA_VERSION-dev
+
+#
+## Lua Rocks
+#
+wget http://luarocks.org/releases/luarocks-$LUA_ROCKS_VERSION.tar.gz -nc -P $TMP
+tar -C $TMP -zxpf $TMP/luarocks-$LUA_ROCKS_VERSION.tar.gz
+cd $TMP/luarocks-$LUA_ROCKS_VERSION && ./configure && sudo make bootstrap
