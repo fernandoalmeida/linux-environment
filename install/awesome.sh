7f1673aef83921effcc32058471601de35699657
Fernando Almeida <fernando@fernandoalmeida.net>
2015-07-02 04:05:45 -0300

    Add `awesome` window manager configs

---
+++ install/awesome.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -le
+
+sudo add-apt-repository --yes ppa:klaus-vormweg/awesome
+sudo apt-get -qq update
+sudo apt-get install --yes --force-yes awesome
+
+ln -sfn $(pwd)/dotfiles/config/awesome $HOME/.config/
