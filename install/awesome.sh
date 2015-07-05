fcd16db9d821d6ab42583a3bc24549b1feba6519
Fernando Almeida <fernando@fernandoalmeida.net>
2015-07-05 06:15:06 -0300

    [awesome] Add `awesome-extras` to install script

--- install/awesome.sh
+++ install/awesome.sh
@@ -2,6 +2,6 @@
 
 sudo add-apt-repository --yes ppa:klaus-vormweg/awesome
 sudo apt-get -qq update
-sudo apt-get install --yes --force-yes awesome
+sudo apt-get install --yes --force-yes awesome awesome-extra
 
 ln -sfn $(pwd)/dotfiles/config/awesome $HOME/.config/
