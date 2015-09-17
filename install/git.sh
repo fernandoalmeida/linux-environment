f4561223debc44a82f57ab75f9461b25270c0ba1
Fernando Almeida <fernando@fernandoalmeida.net>
2015-09-17 01:13:04 -0300

    [git] Install last Git version

--- install/git.sh
+++ install/git.sh
@@ -1,3 +1,5 @@
 #!/bin/sh -e
 
-sudo apt-get install git-core --yes --force-yes -qq
+sudo add-apt-repository ppa:git-core/ppa -y
+sudo apt-get update -qq
+sudo apt-get install git git-man --yes --force-yes -qq
