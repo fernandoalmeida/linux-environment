8e4e866150db695f71f75ff27d1205f04604cd6f
Fernando Almeida <fernando@fernandoalmeida.net>
2015-10-13 01:09:32 -0300

    [lua] Install "Busted" unit testint framework

--- install/lua.sh
+++ install/lua.sh
@@ -15,3 +15,5 @@ sudo apt-get install lua$LUA_VERSION liblua$LUA_VERSION-dev
 wget http://luarocks.org/releases/luarocks-$LUA_ROCKS_VERSION.tar.gz -nc -P $TMP
 tar -C $TMP -zxpf $TMP/luarocks-$LUA_ROCKS_VERSION.tar.gz
 cd $TMP/luarocks-$LUA_ROCKS_VERSION && ./configure && sudo make bootstrap
+
+sudo luarocks install busted
