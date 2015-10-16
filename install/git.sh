7876651d4f37425916cb8d7cd2f51eafcd8e8c42
Fernando Almeida <fernando.santos@locaweb.com.br>
2015-10-16 13:23:14 -0300

    [git] Install Git by compilation mode

--- install/git.sh
+++ install/git.sh
@@ -1,5 +1,14 @@
-#!/bin/sh -e
+#!/bin/bash -e
 
-sudo add-apt-repository ppa:git-core/ppa -y
-sudo apt-get update -qq
-sudo apt-get install git git-man --yes --force-yes -qq
+: ${GITVERSION:=2.6.1}
+: ${PREFIX=/usr/local}
+
+sudo apt-get install libexpat-dev libcurl4-gnutls-dev
+pushd $(mktemp -d)
+  wget -q https://www.kernel.org/pub/software/scm/git/git-${GITVERSION}.tar.gz
+  tar -zxf git-${GITVERSION}.tar.gz
+  cd git-${GITVERSION}
+  make prefix=${PREFIX}
+  sudo make prefix=${PREFIX} install
+  ln -sf ${PREFIX}/bin/git /usr/bin/git
+popd
