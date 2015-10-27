4c5bdf5be983c6b4fc3cf59920ff59cda3c3ca0b
Fernando Almeida <fernando@fernandoalmeida.net>
2015-10-26 23:40:52 -0200

    [git] Changes to Debian 8 (testing)

--- install/git.sh
+++ install/git.sh
@@ -1,14 +1,3 @@
 #!/bin/bash -e
 
-: ${GITVERSION:=2.6.1}
-: ${PREFIX=/usr/local}
-
-sudo apt-get install libexpat-dev libcurl4-gnutls-dev
-pushd $(mktemp -d)
-  wget -q https://www.kernel.org/pub/software/scm/git/git-${GITVERSION}.tar.gz
-  tar -zxf git-${GITVERSION}.tar.gz
-  cd git-${GITVERSION}
-  make prefix=${PREFIX}
-  sudo make prefix=${PREFIX} install
-  ln -sf ${PREFIX}/bin/git /usr/bin/git
-popd
+sudo apt-get install -qqy git
