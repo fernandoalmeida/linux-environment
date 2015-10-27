1ae59f06b2974018723017f560bf6d1ac03c846e
Fernando Almeida <fernando@fernandoalmeida.net>
2015-10-26 23:41:24 -0200

    [awesome] Changes to Debian 8 (testing)

--- install/awesome.sh
+++ install/awesome.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -le
 
-sudo add-apt-repository --yes ppa:klaus-vormweg/awesome
-sudo apt-get -qq update
 sudo apt-get install -qqy awesome awesome-extra
 
 ln -sfn $(pwd)/dotfiles/config/awesome $HOME/.config/
