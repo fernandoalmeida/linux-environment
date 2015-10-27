a1b5dbab7434c5410cf42c47d4c51d30a7955474
Fernando Almeida <fernando@fernandoalmeida.net>
2015-10-26 23:43:03 -0200

    [fortinet] Add comment

--- install/fortinet_client.sh
+++ install/fortinet_client.sh
@@ -3,6 +3,16 @@
 # Thanks to Rene by the Debian packaging:
 # https://hadler.me/linux/forticlient-sslvpn-deb-packages/
 
+: '
+-----BEGIN PGP MESSAGE-----
+Version: GnuPG v1
+
+jA0EAwMCBRaR6bnKKwFgyUUcZ9zrE34gCOjufgOi4lHPQlPeQroU9wn355iAxQzv
+ZqnQs+JT2sX1DZuEgEDJCPW8H0aaVmnMBmidqhxMMzm2hS2HUT8=
+=zC9t
+-----END PGP MESSAGE-----
+'
+
 deb=forticlient-sslvpn_4.4.2317-1_amd64.deb
 wget -q https://hadler.me/files/$deb -P /tmp/
 sudo dpkg -i /tmp/$deb
