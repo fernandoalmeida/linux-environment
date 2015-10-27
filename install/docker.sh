1edf39953f143ff74de0e53ed40f225987dda7dd
Fernando Almeida <fernando@fernandoalmeida.net>
2015-10-26 23:41:05 -0200

    [docker] Changes to Debian 8 (testing)

--- install/docker.sh
+++ install/docker.sh
@@ -1,13 +1,4 @@
 #!/bin/sh -e
 
-#
-## Docker
-#
-wget -qO- https://get.docker.com/ | sh
+sudo apt-get install -qqy docker.io docker-compose
 sudo usermod -aG docker `id -un`
-
-#
-## Compose
-#
-sudo curl -L https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
