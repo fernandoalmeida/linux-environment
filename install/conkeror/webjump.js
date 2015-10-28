456e37407c4085625ee215b2959cf819d6a39298
Fernando Almeida <fernando@fernandoalmeida.net>
2015-10-28 02:22:45 -0200

    [conkeror] Add optional webjump params

--- install/conkeror/webjump.js
+++ install/conkeror/webjump.js
@@ -1,15 +1,20 @@
 define_delicious_webjumps('fernandoalmeida');
 
-define_webjump("gmail", "https://mail.google.com");
+define_webjump("gmail", "https://mail.google.com/mail/u/0/#search/%s");
 define_webjump("news", "https://news.ycombinator.com");
 define_webjump("dropbox", "https://www.dropbox.com/home");
 define_webjump("ifttt", "https://ifttt.com/myrecipes/personal");
-define_webjump("evernote", "https://www.evernote.com/Home.action");
+define_webjump("en", "https://www.evernote.com/Home.action#x=%s",
+	      $alternative = "https://www.evernote.com/Home.action");
+define_webjump("t", "http://translate.google.com/translate_t#auto|en|%s");
+define_webjump("i", "http://www.google.com/images?q=%s",
+	       $alternative = "http://www.google.com/imghp");
+define_webjump("gh", "http://github.com/search?q=%s&type=Everything");
 
 // Locaweb stuff
 define_webjump("outlook", "https://outlook.locaweb.com.br");
 define_webjump("jenkins", "https://ci.qaservices.locaweb.com.br");
-define_webjump("code", "https://code.locaweb.com.br");
+define_webjump("code", "https://code.locaweb.com.br/%s");
 define_webjump("servicenow", "https://lwprod.service-now.com/");
 define_webjump("confluence", "http://confluence.locaweb.com.br/");
 define_webjump("rh", "https://rh.locaweb.com.br/");
