919687013a8b54decf0951dd4b86744d9229e1ab
Fernando Almeida <fernando@fernandoalmeida.net>
2015-10-27 23:51:34 -0200

    [awesome] Fix network widget

--- dotfiles/config/awesome/custom/widgets.lua
+++ dotfiles/config/awesome/custom/widgets.lua
@@ -59,8 +59,8 @@ vicious.register(
     vicious.widgets.net,
     text_widget(
        "  NET: ",
-       '<span color="#7F9F7F">${wlan0 up_kb}↑</span>  '..
-       '<span color="#CC9393">${wlan0 down_kb}↓</span>'
+       '<span color="#7F9F7F">${wlp7s0 up_kb}↑</span>  '..
+       '<span color="#CC9393">${wlp7s0 down_kb}↓</span>'
     ),
     3
  )
