df61d744af708b45319d2a6923b649b2ef964302
Fernando Almeida <fernando@fernandoalmeida.net>
2015-10-27 22:13:03 -0200

    [conkeror] Add tabs navigation bindings

--- install/conkeror/tabs.js
+++ install/conkeror/tabs.js
@@ -1,1 +1,18 @@
 require('new-tabs.js')
+
+function define_switch_buffer_key (key, buf_num) {
+    define_key(
+	default_global_keymap,
+	key,
+        function (I) {
+            switch_to_buffer(
+		I.window,
+                I.window.buffers.get_buffer(buf_num)
+	    );
+        }
+    );
+}
+
+for (let i = 0; i < 10; ++i) {
+    define_switch_buffer_key("C-" + String((i+1)%10), i);
+}
