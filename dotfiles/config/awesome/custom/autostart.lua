3d0bae0e778e0868a6eedf75a52ccfcbbf96b0db
Fernando Almeida <fernando@fernandoalmeida.net>
2015-10-29 04:32:51 -0200

    [awesome] Autostart Emacs daemon mode

--- dotfiles/config/awesome/custom/autostart.lua
+++ dotfiles/config/awesome/custom/autostart.lua
@@ -4,4 +4,7 @@ awful.util.spawn_with_shell("xscreensaver -nosplash")
 
 -- Dropbox
 awful.util.spawn_with_shell("dropbox start")
+
+-- Emacs daemon
+awful.util.spawn_with_shell('emacs --daemon')
 --}}}
