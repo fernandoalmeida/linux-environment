837f40af7011bf93d166e0f1279ea05fcae5d49a
Fernando Almeida <fernando@fernandoalmeida.net>
2015-11-10 13:35:35 -0200

    [awesome] Review bindings

--- dotfiles/config/awesome/custom/bindings.lua
+++ dotfiles/config/awesome/custom/bindings.lua
@@ -2,46 +2,22 @@
 globalkeys = awful.util.table.join(
    awful.key(
       { modkey,           },
-      "Left",
-      awful.tag.viewprev
-   ),
-   awful.key(
-      { modkey,           },
-      "Right",
-      awful.tag.viewnext
-   ),
-   awful.key(
-      { modkey,           },
-      "Escape",
-      awful.tag.history.restore
-   ),
-   awful.key(
-      { modkey,           },
       "/",
       function ()
          mymainmenu:toggle()
       end
    ),
+
+   -- Navigation
    awful.key(
-      { modkey, "Control" },
-      "l",
-      function ()
-         awful.util.spawn("xscreensaver-command -lock")
-      end
+      { modkey,           },
+      "p",
+      awful.tag.viewprev
    ),
    awful.key(
-      { modkey, "Control" },
-      "t",
-      function()
-	 awful.util.spawn(
-	    "xinput " ..
-	       "| grep 'maXTouch Digitizer'" ..
-	       "| awk '{print $7}'" ..
-	       "| tr -d 'id=' " ..
-	       "| xargs xinput --disable"
-	 )
-	 naughty.notify({title = 'Touch Screen', text = 'disabled'})
-      end
+      { modkey,           },
+      "n",
+      awful.tag.viewnext
    ),
    awful.key(
       { modkey,           },
@@ -63,57 +39,17 @@ globalkeys = awful.util.table.join(
       end
    ),
    awful.key(
-      { modkey,           },
-      "w",
-      function ()
-         mymainmenu:show()
-      end
-   ),
-
-   -- Layout manipulation
-   awful.key(
-      { modkey, "Shift"   },
+      { modkey, "Control"   },
       "j",
       function ()
          awful.client.swap.byidx(  1)
       end
    ),
    awful.key(
-      { modkey, "Shift"   },
+      { modkey, "Control"   },
       "k",
       function ()
          awful.client.swap.byidx( -1)
-      end
-   ),
-   awful.key(
-      { modkey, "Control" },
-      "j",
-      function ()
-         awful.screen.focus_relative( 1)
-      end
-   ),
-   awful.key(
-      { modkey, "Control" },
-      "k",
-      function ()
-         awful.screen.focus_relative(-1)
-
-      end
-
-   ),
-   awful.key(
-      { modkey,           },
-      "u",
-      awful.client.urgent.jumpto
-   ),
-   awful.key(
-      { modkey,           },
-      "Tab",
-      function ()
-         awful.client.focus.history.previous()
-         if client.focus then
-            client.focus:raise()
-         end
       end
    ),
 
@@ -136,6 +72,7 @@ globalkeys = awful.util.table.join(
       awesome.quit
    ),
 
+   -- Client
    awful.key(
       { modkey,           },
       "l",
@@ -192,11 +129,58 @@ globalkeys = awful.util.table.join(
          awful.layout.inc(layouts, -1)
       end
    ),
-
+   awful.key(
+      { modkey, },
+      "f",
+      function ()
+         awful.client.fullscreen = not awful.client.fullscreen
+      end
+   ),
+   awful.key(
+      { modkey, "Shift"   },
+      "c",
+      function ()
+         awful.client:kill()
+      end
+   ),
    awful.key(
       { modkey, "Control" },
-      "n",
-      awful.client.restore
+      "space",
+      awful.client.floating.toggle
+   ),
+   awful.key(
+      { modkey, "Control" },
+      "Return",
+      function ()
+         awful.client:swap(awful.client.getmaster())
+      end
+   ),
+   awful.key(
+      { modkey,           },
+      "o",
+      awful.client.movetoscreen
+   ),
+   awful.key(
+      { modkey,           },
+      "t",
+      function ()
+         awful.client.ontop = not awful.client.ontop
+      end
+   ),
+   awful.key(
+      { modkey,           },
+      "m",
+      function ()
+         awful.client.minimized = true
+      end
+   ),
+   awful.key(
+      { modkey, "Shift"   },
+      "m",
+      function ()
+         awful.client.maximized_horizontal = not awful.client.maximized_horizontal
+         awful.client.maximized_vertical   = not awful.client.maximized_vertical
+      end
    ),
 
    -- Prompt
@@ -242,61 +226,26 @@ globalkeys = awful.util.table.join(
       function()
 	 awful.util.spawn("amixer set Master toggle", false)
       end
-   )
-)
-
-clientkeys = awful.util.table.join(
-   awful.key(
-      { modkey, },
-      "f",
-      function (c)
-         c.fullscreen = not c.fullscreen
-      end
    ),
    awful.key(
-      { modkey, "Shift"   },
-      "c",
-      function (c)
-         c:kill()
+      { modkey, "Control", "Shift" },
+      "l",
+      function ()
+         awful.util.spawn("xscreensaver-command -lock")
       end
    ),
    awful.key(
       { modkey, "Control" },
-      "space",
-      awful.client.floating.toggle
-   ),
-   awful.key(
-      { modkey, "Control" },
-      "Return",
-      function (c)
-         c:swap(awful.client.getmaster())
-      end
-   ),
-   awful.key(
-      { modkey,           },
-      "o",
-      awful.client.movetoscreen
-   ),
-   awful.key(
-      { modkey,           },
       "t",
-      function (c)
-         c.ontop = not c.ontop
-      end
-   ),
-   awful.key(
-      { modkey,           },
-      "n",
-      function (c)
-         c.minimized = true
-      end
-   ),
-   awful.key(
-      { modkey,           },
-      "m",
-      function (c)
-         c.maximized_horizontal = not c.maximized_horizontal
-         c.maximized_vertical   = not c.maximized_vertical
+      function()
+	 awful.util.spawn(
+	    "xinput " ..
+	       "| grep 'maXTouch Digitizer'" ..
+	       "| awk '{print $7}'" ..
+	       "| tr -d 'id=' " ..
+	       "| xargs xinput --disable"
+	 )
+	 naughty.notify({title = 'Touch Screen', text = 'disabled'})
       end
    )
 )
@@ -305,79 +254,45 @@ clientkeys = awful.util.table.join(
 for i = 1, 9 do
    globalkeys = awful.util.table.join(
       globalkeys,
-      -- View tag only.
       awful.key(
-         { modkey },
-         "#" .. i + 9,
-         function ()
-            local screen = mouse.screen
-            local tag = awful.tag.gettags(screen)[i]
-            if tag then
-               awful.tag.viewonly(tag)
-            end
-         end
-      ),
-      -- Toggle tag.
-      awful.key(
-         { modkey, "Control" },
-         "#" .. i + 9,
-         function ()
-            local screen = mouse.screen
-            local tag = awful.tag.gettags(screen)[i]
-            if tag then
-               awful.tag.viewtoggle(tag)
-            end
-         end
-      ),
-      -- Move client to tag.
-      awful.key(
-         { modkey, "Shift" },
-         "#" .. i + 9,
-         function ()
-            if client.focus then
-               local tag = awful.tag.gettags(client.focus.screen)[i]
-               if tag then
-                  awful.client.movetotag(tag)
-               end
-            end
-         end
-      ),
-      -- Toggle tag.
-      awful.key(
-         { modkey, "Control", "Shift" },
-         "#" .. i + 9,
-         function ()
-            if client.focus then
-               local tag = awful.tag.gettags(client.focus.screen)[i]
+	 { modkey },
+	 "#" .. i + 9,
+	 function ()
+	    local screen = mouse.screen
+	    local tag = awful.tag.gettags(screen)[i]
 
-               if tag then
-                  awful.client.toggletag(tag)
-               end
-            end
-         end
+	    if tag then
+	       awful.tag.viewonly(tag)
+	    end
+	 end
+      ),
+      awful.key(
+	 { modkey, "Control" },
+	 "#" .. i + 9,
+	 function ()
+	    local screen = mouse.screen
+	    local tag = awful.tag.gettags(screen)[i]
+
+	    if tag then
+	       awful.tag.viewtoggle(tag)
+	    end
+	 end
+      ),
+      awful.key(
+	 { modkey, "Shift" },
+	 "#" .. i + 9,
+	 function ()
+	    if client.focus then
+	       local tag = awful.tag.gettags(client.focus.screen)[i]
+
+	       if tag then
+		  awful.client.movetotag(tag)
+	       end
+	    end
+	 end
       )
    )
 end
-
-clientbuttons = awful.util.table.join(
-   awful.button(
-      { },
-      1,
-      function (c)
-         client.focus = c; c:raise()
-      end
-   ),
-   awful.button(
-      { modkey },
-      1,
-      awful.mouse.client.move
-   ),
-   awful.button(
-      { modkey },
-      3,
-      awful.mouse.client.resize
-   )
-)
 
 root.keys(globalkeys)
 -- }}}
