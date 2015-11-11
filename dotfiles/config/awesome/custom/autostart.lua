bde01a4de67be040f2ed00e1e9970c8355dd29ed
Fernando Almeida <fernando@fernandoalmeida.net>
2015-11-11 01:22:38 -0200

    [awesome] Force keyboard layout

--- dotfiles/config/awesome/custom/autostart.lua
+++ dotfiles/config/awesome/custom/autostart.lua
@@ -7,4 +7,9 @@ awful.util.spawn_with_shell("dropbox start")
 
 -- Emacs daemon
 awful.util.spawn_with_shell('emacs --daemon')
+
+-- Force keyboard layout
+awful.util.spawn_with_shell(
+   'setxkbmap -rules evdev -model evdev -layout us -variant altgr-intl'
+)
 --}}}
