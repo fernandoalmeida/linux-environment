c9e0a8f065c40a9c185c760671fa6860238ef47a
Fernando Almeida <fernando@fernandoalmeida.net>
2015-11-11 01:17:33 -0200

    [awesome] Fix client rules

--- dotfiles/config/awesome/custom/bindings.lua
+++ dotfiles/config/awesome/custom/bindings.lua
@@ -326,6 +326,8 @@ awful.rules.rules = {
    {
       rule = { },
       properties = {
+         border_width = beautiful.border_width,
+         border_color = beautiful.border_normal,
 	 keys = clientkeys,
 	 buttons = clientbuttons
       }
