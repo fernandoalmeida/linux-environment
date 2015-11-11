c91f52dab9cdcdbb2e8671a637b4d7db58ea46ac
Fernando Almeida <fernando@fernandoalmeida.net>
2015-11-10 23:30:06 -0200

    [neo4j] Install `graphviz` to draw graphs

--- install/neo4j.sh
+++ install/neo4j.sh
@@ -5,4 +5,4 @@ wget -qO - https://debian.neo4j.org/neotechnology.gpg.key \
 echo 'deb http://debian.neo4j.org/repo stable/' \
     | sudo tee /etc/apt/sources.list.d/neo4j.list &> /dev/null
 sudo apt-get update -qq
-sudo apt-get install -qqy neo4j
+sudo apt-get install -qqy neo4j graphviz
