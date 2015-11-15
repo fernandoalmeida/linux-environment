15c2ddd09ab1f3233e00e8355f9a65a89e0f3830
Fernando Almeida <fernando@fernandoalmeida.net>
2015-11-14 23:29:14 -0200

    [bash] Fix references to rcfiles

--- install/bash.sh
+++ install/bash.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
-mkdir -p $HOME/.bashrc.d/
-ln -sf $(pwd)/install/bash/bashrc.d $HOME/.bashrc.d
+mkdir -p $HOME/.bashrc.d
+
+for f in $(pwd)/install/bash/bashrc.d/* ; do ln -sf $f $HOME/.bashrc.d/ ; done
 ln -sf $(pwd)/install/bash/bashrc $HOME/.bashrc
 ln -sf $(pwd)/install/bash/inputrc $HOME/.inputrc
