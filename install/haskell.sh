ee2820a6ed19e39a6083ea173c9a2924ea6a722e
Fernando Almeida <fernando@fernandoalmeida.net>
2015-11-14 23:56:52 -0200

    [haskell] Configure `cabal` and install packages
    - Stylish Haskell
    - Happy
    - Hasktags

--- install/haskell.sh
+++ install/haskell.sh
@@ -1,6 +1,16 @@
-#!/bin/sh -e
+#!/bin/bash -e
 
 # The recommend way to install Haskell on your computer is through the
-# Haskell Platform.
-# https://wiki.haskell.org/Implementations
+# Haskell Platform (https://wiki.haskell.org/Implementations)
 sudo apt-get install -qqy haskell-platform
+ln -sf $(pwd)/install/haskell/haskellrc $HOME/.bashrc.d/
+
+if [ ! -f $HOME/.cabal/packages/hackage.haskell.org/00-index.cache ] ; then
+    cabal update
+fi
+
+command -v stylish-haskell || cabal install stylish-haskell
+command -v happy || cabal install happy
+command -v hasktags || cabal install hasktags
+
+ln -sf $(pwd)/install/haskell/stylish-haskell.yaml $HOME/.stylish-haskell.yaml
