747dc7d0ea26c5ac84638df58c072411cc58f95b
Fernando Almeida <fernando@fernandoalmeida.net>
2015-12-05 02:47:46 -0200

    [conkeror] Remove key bindings to changing tabs

--- install/conkeror/tabs.js
+++ install/conkeror/tabs.js
@@ -1,20 +1,2 @@
 require('new-tabs.js');
-
 tab_bar_mode(false);
-
-function define_switch_buffer_key (key, buf_num) {
-    define_key(
-	default_global_keymap,
-	key,
-        function (I) {
-            switch_to_buffer(
-		I.window,
-                I.window.buffers.get_buffer(buf_num)
-	    );
-        }
-    );
-}
-
-for (let i = 0; i < 10; ++i) {
-    define_switch_buffer_key("C-" + String((i+1)%10), i);
-}
