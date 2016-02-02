3242b43bd0c4ed5abef2dfd61182831a548f6ca3
Fernando Almeida <fernando@fernandoalmeida.net>
2016-02-02 00:31:51 -0300

    [nodejs] Refactor installation script

--- install/nodejs.sh
+++ install/nodejs.sh
@@ -1,3 +1,10 @@
 #!/bin/bash -e
 
-sudo apt-get install -qqy nodejs nodejs-legacy npm
+curl -qqo- https://raw.githubusercontent.com/creationix/nvm/master/install.sh \
+    | bash
+
+ln -sfn $(pwd)/install/nodejs/bashrc $HOME/.bashrc.d/nodejs
+source $(pwd)/install/nodejs/bashrc
+
+nvm install stable
+nvm alias default stable
