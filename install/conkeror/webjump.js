ae1aaed7ea855354c49d06ba2365448d60e7854d
Fernando Almeida <fernando@fernandoalmeida.net>
2016-02-19 10:05:45 -0300

    [conkeror] Add some webjumps

--- install/conkeror/webjump.js
+++ install/conkeror/webjump.js
@@ -4,16 +4,17 @@ define_webjump("gmail", "https://mail.google.com/mail/u/0/#search/%s");
 define_webjump("news", "https://news.ycombinator.com");
 define_webjump("dropbox", "https://www.dropbox.com/home");
 define_webjump("ifttt", "https://ifttt.com/myrecipes/personal");
-define_webjump("en", "https://www.evernote.com/Home.action#x=%s",
+define_webjump("evernote", "https://www.evernote.com/Home.action#x=%s",
 	      $alternative = "https://www.evernote.com/Home.action");
 define_webjump("t", "http://translate.google.com/translate_t#auto|en|%s");
 define_webjump("i", "http://www.google.com/images?q=%s",
 	       $alternative = "http://www.google.com/imghp");
-define_webjump("gh", "http://github.com/search?q=%s&type=Everything");
+define_webjump("github", "http://github.com/search?q=%s&type=Everything");
+define_webjump("whatsapp", "https://web.whatsapp.com");
 
 // Locaweb stuff
 define_webjump("outlook", "https://outlook.locaweb.com.br");
-define_webjump("ci", "http://ci.qaservices.locaweb.com.br");
+define_webjump("jenkins", "http://ci.qaservices.locaweb.com.br");
 define_webjump("code", "https://code.locaweb.com.br/%s");
 define_webjump("servicenow", "https://lwprod.service-now.com/");
 define_webjump("confluence", "http://confluence.locaweb.com.br/");
@@ -30,5 +31,9 @@ define_webjump('portal-locaweb', 'https://portallocaweb.cservices.com.br/PortalL
 define_webjump('ponto-online', 'https://portallocaweb.cservices.com.br/WebPontoLocaweb/just_user/IncluirMarcacaoOnLine.asp')
 define_webjump('espelho-de-ponto', 'https://portallocaweb.cservices.com.br/WebPontoLocaweb/Relatorios/SolicitaRelatorio.asp?Relatorio=RELRB118')
 define_webjump('campfire', 'https://loca.campfirenow.com/')
+define_webjump(
+    'leela',
+    'http://webleela.service.ita.consul.locaweb.com.br/' // 10.30.124.172
+)
 
 webjumps.g = webjumps.google;
