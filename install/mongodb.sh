36832258e3c0614a08b56fde9f39aa6f1272780e
Fernando Almeida <fernando@fernandoalmeida.net>
2016-03-24 16:12:40 -0300

    [mongodb] Change to Debian Jessie repo

--- install/mongodb.sh
+++ install/mongodb.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -e
 
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
-echo "deb http://repo.mongodb.org/apt/debian wheezy/mongodb-org/3.0 main" | sudo tee /etc/apt/sources.list.d/mongodb.list
+echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/3.3 main" \
+    | sudo tee /etc/apt/sources.list.d/mongodb.list
 sudo apt-get update -qq
 sudo apt-get install -qqy mongodb-org
