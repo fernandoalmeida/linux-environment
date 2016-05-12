37d83fa00e589617a2f6f3450537d8f34251a79c
Fernando Almeida <fernando@fernandoalmeida.net>
2016-05-12 02:38:52 -0300

    [telegraf] Add installation script

---
+++ install/telegraf.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -euo pipefail
+
+curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+echo "deb https://repos.influxdata.com/debian jessie stable" \
+    | sudo tee /etc/apt/sources.list.d/influxdb.list
+
+sudo apt-get update -qq
+sudo apt-get install -qqy telegraf
+
+sudo systemctl start influxdb
