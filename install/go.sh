79692fe71393e2e39ed3505f99f21aa55135639b
Fernando Almeida <fernando@fernandoalmeida.net>
2016-05-22 23:35:36 -0300

    [go] Update Go version

--- install/go.sh
+++ install/go.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -e
-: ${GOVERSION:="1.5.1"}
+: ${GOVERSION:="1.6.2"}
 
 pushd $(mktemp -d)
+  [ -d /usr/local/go ] && sudo rm -fr /usr/local/go
+
   wget -q \
-     https://storage.googleapis.com/golang/go$GOVERSION.linux-amd64.tar.gz
+       https://storage.googleapis.com/golang/go$GOVERSION.linux-amd64.tar.gz
+
   sudo tar -C /usr/local -xzf go$GOVERSION.linux-amd64.tar.gz
+  sudo ln -nsf /usr/local/go/bin/go /usr/bin/go
 popd
 
 ln -nsf $(pwd)/install/go/bashrc $HOME/.bashrc.d/go
