0bfd6b655047fbaf4df79b8b2fbe0bfe95614073
Fernando Almeida <fernando@fernandoalmeida.net>
2016-08-22 22:55:50 -0300

    [conkeror] Remove PDF handler

--- install/conkeror/handlers.js
+++ install/conkeror/handlers.js
@@ -1,2 +1,2 @@
-content_handlers.set("application/pdf", content_handler_open_default_viewer);
-external_content_handlers.set("application/pdf", "evince");
+// content_handlers.set("application/pdf", content_handler_open_default_viewer);
+// external_content_handlers.set("application/pdf", "evince");
