630ffa48d7c6aca26ec7f1917ec7606aa8c9175b
Fernando Almeida <fernando@fernandoalmeida.net>
2016-11-03 03:20:28 -0300

    [docker] Add `docker-machine` on `docker` script

--- install/docker.sh
+++ install/docker.sh
@@ -1,5 +1,17 @@
-#!/bin/sh -e
+#!/bin/bash -e
+
+os=`uname -s`
+arch=`uname -m`
+machine_repo=https://github.com/docker/machine/releases/download/
+machine_version=0.8.2
+machine_file=docker-machine-$os-$arch
 
 sudo apt-get install -qqy docker.io docker-compose
 sudo usermod -aG docker `id -un`
-cp -fr {$(pwd)/install/docker,}/etc/default/docker
+sudo cp -fr {$(pwd)/install/docker,}/etc/default/docker
+
+pushd $(mktemp -d)
+  curl -sqLo docker-machine $machine_repo/v$machine_version/$machine_file
+  chmod +x docker-machine
+  sudo mv docker-machine /usr/local/bin/
+popd
