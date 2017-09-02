d7ac4a0888d9b833ef7ed1e0c1b0513acc95e4a4
Fernando Almeida <fernando@fernandoalmeida.net>
2017-09-02 14:39:05 -0300

    [conkeror] Improve webjumps

--- install/conkeror/webjump.js
+++ install/conkeror/webjump.js
@@ -13,14 +13,6 @@ define_webjump("i", "http://www.google.com/images?q=%s",
 	       $alternative = "http://www.google.com/imghp");
 define_webjump("github", "http://github.com/search?q=%s&type=Everything");
 define_webjump("whatsapp", "https://web.whatsapp.com");
-define_webjump(
-    "ruby",
-    function(klass) {
-	var url = 'http://ruby-doc.org/core-2.2.0/'
-	return url + klass.charAt(0).toUpperCase() + klass.slice(1) + '.html';
-    },
-    $alternative = 'http://ruby-doc.org/core-2.2.0/'
-)
 define_webjump("hangout", "https://plus.google.com/hangouts/_/");
 
 // Locaweb stuff
@@ -32,9 +24,9 @@ define_webjump("confluence", "http://confluence.locaweb.com.br/");
 define_webjump("rh", "http://rh.locaweb.com.br/");
 define_webjump("basecamp", "https://basecamp.com/1739668/projects/9206881");
 define_webjump("kanbanery", "https://paaslocaweb.kanbanery.com/projects/45757/board/");
-define_webjump('jira', 'https://locaweb.atlassian.net/browse/NPD')
-define_webjump('fila-beta', 'http://fila-beta.hospedagem.locaweb.com.br/LogGeral.aspx?prov=%s')
-define_webjump('sistema-beta', 'sistema-beta.hospedagem.locaweb.com.br')
+define_webjump('jira', 'https://locaweb.atlassian.net/browse/%s')
+define_webjump('fila', 'http://fila-beta.hospedagem.locaweb.com.br/LogGeral.aspx?prov=%s')
+define_webjump('si', 'sistema-beta.hospedagem.locaweb.com.br')
 
 function locaweb_url(protocol, subdomain, env) {
     var prefix = {
@@ -59,7 +51,7 @@ define_webjump(
     function(env) { return locaweb_url('https', 'painelhospedagem', env) }
 );
 define_webjump(
-    'prov-workers',
+    'prov',
     function(env) { return locaweb_url('http', 'prov-workers.hospedagem', env) }
 );
 
@@ -67,10 +59,6 @@ define_webjump('portal-locaweb', 'https://portallocaweb.cservices.com.br/PortalL
 define_webjump('ponto-online', 'https://portallocaweb.cservices.com.br/WebPontoLocaweb/just_user/IncluirMarcacaoOnLine.asp')
 define_webjump('ponto-espelho', 'https://portallocaweb.cservices.com.br/WebPontoLocaweb/Relatorios/SolicitaRelatorio.asp?Relatorio=RELRB118')
 define_webjump('campfire', 'https://loca.campfirenow.com/')
-define_webjump(
-    'leela',
-    'http://webleela.service.ita.consul.locaweb.com.br/' // 10.30.124.172
-)
-define_webjump('grafana', 'https://grafana.locaweb.com.br/')
+define_webjump('grafana', 'https://dashboards.locaweb.com.br/')
 
 webjumps.g = webjumps.google;
