fa8d6f28a79024fcfcbb1bff74eec9f8a9c8a463
Fernando Almeida <fernando@fernandoalmeida.net>
2017-10-26 21:50:29 -0300

    [conkeror] Add updatable Debian nightly repo

--- install/conkeror.sh
+++ install/conkeror.sh
@@ -1,4 +1,10 @@
 #!/bin/bash -e
 
-sudo apt-get install -y conkeror
+echo <<EOF | sudo tee /etc/apt/sources.list.d/conkeror.list
+deb     http://noone.org/conkeror-nightly-debs jessie main
+deb-src http://noone.org/conkeror-nightly-debs jessie main
+EOF
+
+sudo apt-get update -qq
+sudo apt-get install -qqy conkeror
 ln -sf $(pwd)/install/conkeror/conkerorrc $HOME/.conkerorrc
