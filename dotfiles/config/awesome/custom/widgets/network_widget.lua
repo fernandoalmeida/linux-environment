bdf179ad0f604c6349b2fc0724c4de0be6328082
Fernando Almeida <fernando@fernandoalmeida.net>
2018-05-05 04:32:21 -0300

    [awesome] Refactor network widget

--- dotfiles/config/awesome/custom/widgets/network_widget.lua
+++ dotfiles/config/awesome/custom/widgets/network_widget.lua
@@ -1,17 +1,21 @@
 function network_widget()
    local widget = wibox.widget.textbox()
-   local interface = awful.util.pread(
-      "ip -f inet route | awk '/default/ { print $5 }'"
-   ):gsub("[^a-za-Z0-9]", "")
 
-   vicious.register(
-      widget,
-      vicious.widgets.net,
-      text_widget(
-	 '  <span color="#7F9F7F">${'.. interface ..' up_kb}↑</span>'..
-	 '  <span color="#CC9393">${'.. interface ..' down_kb}↓</span>'
-      ),
-      3
+   awful.spawn.easy_async(
+      'bash -c "ip -f inet route | grep default | cut -d \' \' -f 5"',
+      function(stdout, stderr, reason, exit_code)
+	 local interface = stdout:gsub('[^a-zA-Z0-9]', '');
+
+         vicious.register(
+            widget,
+            vicious.widgets.net,
+            text_widget(
+               '  <span color="#7F9F7F">${'.. interface ..' up_kb}↑</span>'..
+               '  <span color="#CC9393">${'.. interface ..' down_kb}↓</span>'
+            ),
+            5
+         )
+      end
    )
 
    return widget
