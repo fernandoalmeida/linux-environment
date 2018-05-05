7025bac1405aa8ba5e243159fa6259e4e96587b1
Fernando Almeida <fernando@fernandoalmeida.net>
2018-05-05 03:04:17 -0300

    [awesome] Remove emacs daemon auto start

--- dotfiles/config/awesome/custom/autostart.lua
+++ dotfiles/config/awesome/custom/autostart.lua
@@ -5,8 +5,6 @@ awful.util.spawn_with_shell("xscreensaver -nosplash")
 -- Dropbox
 awful.util.spawn_with_shell("dropbox start")
 
--- Emacs daemon
-awful.util.spawn_with_shell('emacs --daemon')
 
 -- Setup keyboard
 awful.util.spawn_with_shell('keyboard-setup')
