692e1a515a7add27dcaeb21951c0aea319312d1b
Fernando Almeida <fernando@fernandoalmeida.net>
2019-04-05 11:43:28 -0300

    [arch][wakatime] Add wakatime CLI configuration scripts

--- install/arch/wakatime.sh
+++ install/arch/wakatime.sh
@@ -1,3 +1,10 @@
 #!/bin/bash -e
 
-sudo aura -Ay wakatime
+sudo pip install wakatime
+
+cp $(pwd)/install/arch/wakatime/config $HOME/.wakatime.cfg
+
+cat <<MESSAGE
+Get the API key on https://wakatime.com/settings/api-key
+and replace $WAKATIME_APY_KEY on $HOME/.wakatime.cfg
+MESSAGE
