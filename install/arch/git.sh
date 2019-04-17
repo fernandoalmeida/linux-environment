1ba02c65e97ffa4cc0d6d3ac43fc3733c0070be9
Fernando Almeida <fernando@fernandoalmeida.net>
2019-04-17 16:33:19 -0300

    [arch][git] Add default global commit template
    **What is the content type?**
    
    - Improvement
    
    **Why is this change necessary?**
    
    - It improves the development process, turning easier to write the content of
      the pull request (getting automatically the content of the commit) and turning
      the content of the pull request greppable locally in the future if necessary.
    
    **How does it address the issue?**
    
    - Setup the template of the pull request as default template of the commit.
    
    **Links**
    
    - https://git-scm.com/book/en/v2/Customizing-Git-Git-Configuration#_code_commit_template_code

--- install/arch/git.sh
+++ install/arch/git.sh
@@ -4,3 +4,4 @@ sudo pacman -Sy git
 
 ln -sf $(pwd)/install/arch/git/gitconfig $HOME/.gitconfig
 ln -sf $(pwd)/install/arch/git/gitignore_global $HOME/.gitignore_global
+ln -sf $(pwd)/install/arch/git/commit-template $HOME/.commit-template
