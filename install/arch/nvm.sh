025e2879cd2b687e9a1b326c61616985bc4c0634
Fernando Almeida <fernando@fernandoalmeida.net>
2020-03-13 12:43:15 -0300

    [arch][typescript] Add language server configuration

--- install/arch/nvm.sh
+++ install/arch/nvm.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -e
 
 sudo aura -Ay nvm
+
+# language server
+sudo npm i -g typescript-language-server
+sudo npm i -g typescript
+
 ln -sfn $(pwd)/install/arch/nvm/nvmrc $HOME/.bashrc.d/custom/
