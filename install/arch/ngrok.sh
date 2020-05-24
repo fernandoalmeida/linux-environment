a7b310d1fe4530375db9ff7b6c37687aaac1d6e4
Fernando Almeida <fernando@fernandoalmeida.net>
2020-05-24 13:31:48 -0300

    [arch][ngrok] Add installation script

---
+++ install/arch/ngrok.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+sudo aura -A ngrok
+
+: '
+-----BEGIN PGP MESSAGE-----
+
+jA0EBwMCuNaRmGoLGzby0ngBkgJeU/pz/PDiUrhz48vp3BTWDwWjO1NdWPxILPsm
+32t1eaQ44rBYxpecJ+879Ssc1C7AtuRz/2USIvCcVMNxzXuQV6qE7iM7S6AOHtpU
+GDzXm8DF64QsA26PQascn33uhQQ+4i2fZVrsyMuUheFwPo55y+P7syQ=
+=wXhN
+-----END PGP MESSAGE-----
+'
