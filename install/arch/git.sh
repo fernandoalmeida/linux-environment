d9212908ee5e4ba36fc39265ab2653e222c88eda
Fernando Almeida <fernando@fernandoalmeida.net>
2020-09-14 21:39:52 -0300

    [arch][git] Remove commit template

--- install/arch/git.sh
+++ install/arch/git.sh
@@ -4,4 +4,3 @@ sudo pacman -Sy git
 
 ln -sf $(pwd)/install/arch/git/gitconfig $HOME/.gitconfig
 ln -sf $(pwd)/install/arch/git/gitignore_global $HOME/.gitignore_global
-ln -sf $(pwd)/install/arch/git/commit-template $HOME/.commit-template
