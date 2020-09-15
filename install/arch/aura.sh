b8df9d90a2016c33d4a8dc36c17cc01f9ff7636a
Fernando Almeida <fernando@fernandoalmeida.net>
2020-09-14 21:40:51 -0300

    [arch][aura] Fix aura setup

--- install/arch/aura.sh
+++ install/arch/aura.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -e
 
+sudo pacman -Sy stack
+
 pushd $(mktemp -d)
   git clone https://aur.archlinux.org/aura-git.git
   cd aura-git
   makepkg
-  sudo pacman -U aura-git-*.tar.xz
+  sudo pacman -U aura-git-*.tar.*
 popd
