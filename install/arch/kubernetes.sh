6bd60b305ca5f4dc72a18226d61de9cef002163d
Fernando Almeida <fernando@fernandoalmeida.net>
2020-09-19 19:51:49 -0300

    [arch][kubernetes] Add instructions about config restore

--- install/arch/kubernetes.sh
+++ install/arch/kubernetes.sh
@@ -2,3 +2,10 @@
 
 sudo pacman -Sy kubectl minikube
 sudo aura -A kops
+
+echo "##################################################"
+cat <<EOF
+To restore configurations, use:
+ln -sf /path/to/configurations/.kube ~/
+EOF
+echo "##################################################"
