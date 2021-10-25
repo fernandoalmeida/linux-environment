ec93e45e3a592a9226087a6bff2fd6447cb82259
Fernando Almeida <fernando@fernandoalmeida.net>
2021-10-25 10:09:49 -0300

    [arch][utils] Change direnv installation package

--- install/arch/direnv.sh
+++ install/arch/direnv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-sudo aura -Ay direnv
+sudo aura -Ay direnv-bin
 
 ln -nsf $(pwd)/install/arch/direnv/direnvrc $HOME/.bashrc.d/custom/direnvrc
 source $(pwd)/install/arch/direnv/direnvrc
