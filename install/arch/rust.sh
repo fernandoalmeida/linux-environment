c0a5ce32f9d587b9ecc7492a6c32b95840e65e43
Fernando Almeida <fernando@fernandoalmeida.net>
2021-11-12 16:22:51 -0300

    [arch][rust] Add Emacs integration required tooling

--- install/arch/rust.sh
+++ install/arch/rust.sh
@@ -4,3 +4,8 @@ sudo pacman -Syy rustup
 
 rustup default stable
 rustup component add rustfmt clippy rls
+
+# Emacs integration required tooling
+rustup toolchain add nightly
+rustup component add rust-src
+cargo +nightly install racer
