67b93a88a37c1fbe8a4be3f4ef5d01a94d4efc2c
Fernando Almeida <fernando@fernandoalmeida.net>
2022-07-22 19:23:16 -0300

    [arch][vanta] Add installation script

---
+++ install/arch/vanta.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+sudo aura -A vanta-agent
+
+echo -e "\033[32m"
+echo -e "================================================="
+echo -e "SEE THE ENCRYPTED SETUP INSTRUCTIONS IN THE FILE"
+echo -e "================================================="
+echo -e "\033[0m"
+
+# -----BEGIN PGP MESSAGE-----
+# jA0EBwMCLMid2PvYKLr40qIBgz9QGViM2AFd+1h3gmVdSEfnmK1qb/fRy26/RX40
+# oYwGRWeBMAyk+Z3DeViD9CwI+25KBmBru+0rzLvNoYOCkrWa0szEZ7N1wyF/8kHx
+# 3glcF9Hp4rhdQ74GwTkl3fY8T3hUicYsb7+WTbyoim1yxxCne8tHlppH4o1sL1Gx
+# DNi/Dyyg2ARUcS6w/lZ4tB7cho7wgwizEFq6U2Pdx1ICxT0=
+# =Tgt9
+# -----END PGP MESSAGE-----
