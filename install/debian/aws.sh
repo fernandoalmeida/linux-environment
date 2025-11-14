57feb7d8eb4323f8ce7ff6ba546e65ab97fb6b1a
Fernando Almeida <fernando@fernandoalmeida.net>
2025-11-14 11:19:20 -0300

    [aws] update aws debian setup script

--- install/debian/aws.sh
+++ install/debian/aws.sh
@@ -1,19 +1,7 @@
 #!/bin/bash -e
 
-sudo pip install -q awscli
-
-cat <<EOF
-==================================================
-# $HOME/.aws/credentials
-==================================================
-[default]
-aws_access_key_id=EXAMPLE_DEFAULT_ID
-aws_secret_access_key=EXAMPLE_DEFAULT_KEY
-region=us-west-2
-output=json
-
-[fernando]
-aws_access_key_id=EXAMPLE_USER_ID
-aws_secret_access_key=EXAMPLE_USER_KEY
-==================================================
-EOF
+pushd $(mktemp -d)
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+  unzip awscliv2.zip
+  sudo ./aws/install
+popd
