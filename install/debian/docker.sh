8ec3fd60e7f97a0589b6d4c9651bd6177578ae8e
Fernando Almeida <fernando@fernandoalmeida.net>
2025-11-14 11:20:07 -0300

    [docker] update debian docker setup

--- install/debian/docker.sh
+++ install/debian/docker.sh
@@ -1,17 +1,8 @@
 #!/bin/bash -e
 
-os=`uname -s`
-arch=`uname -m`
-machine_repo=https://github.com/docker/machine/releases/download/
-machine_version=0.8.2
-machine_file=docker-machine-$os-$arch
+sudo apt install -qqy docker.io docker-compose-v2
 
-sudo apt-get install -qqy docker.io docker-compose
-sudo usermod -aG docker `id -un`
-sudo cp -fr {$(pwd)/install/docker,}/etc/default/docker
+sudo groupadd docker
+sudo usermod -aG docker $USER
 
-pushd $(mktemp -d)
-  curl -sqLo docker-machine $machine_repo/v$machine_version/$machine_file
-  chmod +x docker-machine
-  sudo mv docker-machine /usr/local/bin/
-popd
+echo "Log out and log back in or run 'newgrp docker' to activate the group without logging out"
